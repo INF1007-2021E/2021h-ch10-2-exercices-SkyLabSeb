3da8a300aa2fcc92ac34b585d64f1966a402865a
SkyLabSeb <77995832+SkyLabSeb@users.noreply.github.com>
2021-04-01 12:38:36 -0400

    update

--- exercice.py
+++ exercice.py
@@ -2,12 +2,27 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 
 
 # TODO: Définissez vos fonctions ici
+
+def lecture_csv(fichier_csv):
+
+    df = pd.read_csv(fichier_csv, sep=";", header=0)
+    y = df["quality"]
+    x = df.drop(columns=["quality"])
 
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    pass
+
+    liste = lecture_csv("./data/winequality-white.csv")
+    print(liste)
+
+
